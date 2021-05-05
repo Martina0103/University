59a096aa39272ef7b21077dcbaec15ed08eeb659
Martina Markovska <martina-markovska@hotmail.com>
2021-05-05 02:12:36 +0200

    Faza 1 => Zavrshena, Dodadeni se prebaruvanja za Course, Teacher, Student

---
+++ ViewModels/EnrollmentEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using University.Models;
+
+namespace University.ViewModels
+{
+    public class EnrollmentEditViewModel
+    {
+        public Course Course { get; set; }
+        public IEnumerable<int> SelectedStudent { get; set; }
+        //public IEnumerable<SelectListItems> StudentList { get; set; }
+    }
+}
---
+++ ViewModels/StudentSearchViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using University.Models;
+
+namespace University.ViewModels
+{
+    public class StudentSearchViewModel
+    {
+        public IEnumerable<Student> Students { get; set; }
+        public SelectList StudentIds { get; set; } //za indeks na studentot
+        public string StudentStudentId { get; set; } //string vrednost barame
+        public string SearchName { get; set; } //za imeto (FirstName)
+        public string SearchLast { get; set; } // za prezimeto
+    }
+}
